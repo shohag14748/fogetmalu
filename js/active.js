109615919a3c2e3e291d04d5c4fd70c34511f4e9
Mohammad Ali Shohag <47399558+shohag14748@users.noreply.github.com>
2020-03-20 11:28:28 +0600

    up

--- js/active.js
+++ js/active.js
@@ -1,23 +1,48 @@
 $(document).ready(function () {
-$('.slider-inner').owlCarousel({
-    loop: true,
-    nav: false,
-    dots: false,
+//var animateInAll = $('.slider-inner');
+//    animateInAll.owlCarousel({
+//    loop: true,
+//    nav: true,
+//    dots: false,
 //    autoplay: true,
-    navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 1
-        },
-        1000: {
-            items: 1
-        }
-    }
-})
-    
+//    navText: ['<i class="icofont-simple-left"></i>', '<i class="icofont-simple-right"></i>'],
+//    responsive: {
+//        0: {
+//            items: 1
+//        },
+//        600: {
+//            items: 1
+//        },
+//        1000: {
+//            items: 1
+//        }
+//    }
+//});
+//    animateInAll.on("changed.owl-carousel",function(event){
+//        var item=event.item.index-2;
+//        $('h1').removeClass('animate fadeInUp');
+//        $('.owl-item').not('.clone').eq(item).find('h1').addClass('animate slideInRight');
+//    })
+//    
+   
+//var owl = $('.slider-inner').owlCarousel({
+////    animateOut: 'slideOutDown',
+////    animateIn: 'slideInUp',
+//    items:1,
+//    margin:0,
+//    stagePadding:0,
+//    smartSpeed:450,
+//    loop: true,
+//    dots: true,
+//    autoplay: true,
+//});
+//    owl.on('change.owl.carousel', function (event) {
+////    var el = event.target;
+//    $('h1').addClass('slideInRight animated')
+//            .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+//                $('h1').removeClass('slideInUp animated');
+//            });
+//        });
 
 $('.review-slider').owlCarousel({
     loop:true,
@@ -25,6 +50,7 @@ $('.review-slider').owlCarousel({
     margin:30,
     autoHeight:true,
     dotsEach:1,
+    autoplay: true,
     responsive:{
         0:{
             items:1
@@ -43,6 +69,7 @@ $('.feed-slider').owlCarousel({
     margin:0,
     autoHeight:true,
     dotsEach:1,
+    autoplay: true,
     responsive:{
         0:{
             items:2
